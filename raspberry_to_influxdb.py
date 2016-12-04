e8a336e3707b806bfb2f9cd38aa9a1450eefbea5
Park <bbayoung7849@gmail.com>
2016-12-04 14:15:14 +0900

    Update using urllib and Add random value

--- raspberry_to_influxdb.py
+++ raspberry_to_influxdb.py
@@ -1,4 +1,5 @@
-import urllib
+from urllib import request
+import random
 
 url = 'http://52.78.246.225:8086/write?db=mydb'
 
@@ -6,12 +7,13 @@ queries = []
 queries.append('sg_ultrasonic,host=server01 value={0}'.format(100))
 queries.append('sg_temperature,host=server01 value={0}'.format(100))
 queries.append('sg_light,host=server01 value={0}'.format(100))
+queries.append('sg_random,host=server01 value={0}'.format(random.randrange(10, 20)))
 
 queries_str = '\n'.join(queries)
 data = str.encode(queries_str)
 
-request = urllib.request.Request(url, data, {'Content-Type': 'application/octet-stream'}, method='POST')
-response = urllib.request.urlopen(request)
+req = request.Request(url, data, {'Content-Type': 'application/octet-stream'}, method='POST')
+response = request.urlopen(req)
 
 if response.status == 204:
     print('success')
